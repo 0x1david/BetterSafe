c02cea6c621a38b071de4d63117edc289e96badf
0x1david <bousi.david@gmail.com>
2023-09-18 17:50:05 +0200

    process

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,7 +1,8 @@
 pub use super::TerminalCommand;
 use std::collections::HashSet;
 use std::process::exit;
-pub fn parse(args: Vec<String>) {
+
+pub fn parse(args: Vec<String>, terminal_command: &mut TerminalCommand) {
     let arg = match args.get(0) {
         Some(arg) => arg,
         None => return,
@@ -22,6 +23,9 @@ pub fn parse(args: Vec<String>) {
             process_long_arg(&arg[2..]);
         } else if second_char != '-' {
             process_short_arg(&arg[1..]);
+            for character in arg[1..].chars() {
+                terminal_command.add_arg(character)
+            }
         } else {
             eprintln!("No argument given following a '--'.");
             exit(1);
@@ -29,11 +33,14 @@ pub fn parse(args: Vec<String>) {
     } else {
         process_path(&arg);
     }
+    parse(args[1..].to_vec(), terminal_command)
 }
 
-pub fn process_short_arg(arg: &str) -> &str {
+pub fn process_short_arg(arg: &str) -> Vec<char> {
     let allowed_flags = ['j', 'k'];
     let mut previous_flags = HashSet::new();
+    let mut arg_chars = Vec::new();
+
     for flag in arg.chars() {
         if !allowed_flags.contains(&flag) {
             eprintln!("Invalid argument(s) following a '-'.");
@@ -44,8 +51,9 @@ pub fn process_short_arg(arg: &str) -> &str {
             exit(1);
         }
         previous_flags.insert(flag);
+        arg_chars.push(flag);
     }
-    arg
+    arg_chars
 }
 pub fn process_long_arg(arg: &str) {
     unimplemented!();
