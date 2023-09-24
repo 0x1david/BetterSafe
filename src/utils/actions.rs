e803f1732c822462100b9d9115d71ad0c81b246d
0x1david <bousi.david@gmail.com>
2023-09-24 20:13:14 +0200

    Added path alternating function

--- src/utils/actions.rs
+++ src/utils/actions.rs
@@ -1,9 +1,10 @@
-use super::constants::get_archive_dir;
+use super::constants::{get_archive_dir, get_home_dir};
+use dirs::home_dir;
 use std::env;
 use std::fs;
 use std::io;
 use std::io::ErrorKind;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::process::Command;
 
@@ -17,10 +18,10 @@ pub fn version() -> () {
     println!("Current version is '0000.1a'");
 }
 
-pub fn default_action(path: &String) -> () {
+pub fn default_action(path: &String) -> Result<(), E> {
     println!("{} will be archived", path);
     let archive_dir = get_archive_dir();
-    unimplemented!()
+    let home_dir = home_dir()?;
 }
 
 pub fn archive(path: &String) -> () {
@@ -39,7 +40,9 @@ pub fn abandon(path: &String) -> () {
     unimplemented!();
 }
 
+// TODO: Implement alternate path and scale down directory if path is too deep
 pub fn portal() -> io::Result<()> {
+    todo!("Implement alternate path and scale down directory if path is too deep");
     let archive_dir = get_archive_dir();
     let shell = env::var("SHELL").unwrap_or_else(|_| "sh".into());
     match env::set_current_dir(&archive_dir) {
@@ -61,4 +64,38 @@ pub fn portal() -> io::Result<()> {
     }
     Command::new(shell).status()?;
     Ok(())
+}
+
+fn get_alternate_path(cwd_option: Option<PathBuf>) -> String {
+    let cwd = match cwd_option {
+        Some(path) => path,
+        None => match env::current_dir() {
+            Ok(cwd) => cwd,
+            Err(_) => {
+                eprintln!("Cannot properly access the file system on your device.");
+                exit(1);
+            }
+        },
+    };
+
+    let archive_dir = get_archive_dir();
+    let home_dir = get_home_dir();
+    let cwd_str = cwd.to_string_lossy().to_string();
+    let archive_dir_str = archive_dir.to_string_lossy().to_string();
+    let home_dir_str = home_dir.to_string_lossy().to_string();
+
+    if cwd.starts_with(&home_dir) {
+        let target_path = cwd_str.replace(&home_dir_str, &archive_dir_str);
+        return target_path;
+    } else if cwd.starts_with(&archive_dir) {
+        let target_path = cwd_str.replace(&archive_dir_str, &archive_dir_str);
+        return target_path;
+    } else {
+        return archive_dir_str;
+    }
+}
+
+// TODO: Create Directories if the path for the file creation does not exist
+fn chain_create_directories(path: PathBuf) {
+    unimplemented!();
 }
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -6,3 +6,7 @@ pub fn get_archive_dir() -> PathBuf {
         .expect("Unable to determine home directory")
         .join(".archives")
 }
+
+pub fn get_home_dir() -> PathBuf {
+    dirs::home_dir().expect("Cannot properly access the file system on your device.")
+}
